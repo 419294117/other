62ec76bbc616d82a7944aac93d58c2e24eefe17b
419294117 <419294117@qq.com>
2020-06-01 10:06:55 +0800

    up

--- index.js
+++ index.js
@@ -30,6 +30,141 @@
 //   return newObj;
 // }
 
-const a = {};
+// const a = {};
 
-console.log(a?.b ?? 7);
+// console.log(a?.b ?? 7);
+
+// function create(fn, ...args) {
+//   let obj = {};
+//   obj.__proto__ = fn.prototype;
+//   let result = fn.apply(obj, args);
+//   return result instanceof Object ? result : obj;
+// }
+
+// function P(name) {
+//   this.name = name;
+// }
+
+// const a = create(P, '123');
+// console.log(a);
+
+// var obj = {
+//   name: '小鹿',
+//   age: '22',
+//   adress: '小鹿动画学编程',
+// };
+
+// function print() {
+// console.log(this); // 打印 this 的指向
+// console.log(arguments); // 打印传递的参数
+// }
+// print();
+
+// 通过 call 改变 this 指向
+// print.call(obj, 1, 2, 3);
+
+// // 通过 apply 改变 this 指向
+// print.apply(obj, [1, 2, 3]);
+
+// // 通过 bind 改变 this 的指向
+// let fn = print.bind(obj, 1, 2, 3);
+// fn();
+
+// function fn(name) {
+//   this.name = name;
+//   this.type = 'sss';
+// }
+
+// let f = new fn('123');
+// let f1 = new fn('34');
+// f.__proto__.type = "vvv"
+// console.log(f);
+// console.log(f1);
+// console.log(fn.prototype);
+// console.log(f.__proto__);
+
+// console.log(f instanceof fn)
+
+// console.log(a);
+// var a = 1;
+
+// const arr = [1, 2];
+// let b = arr.reduce((acc, item) => {
+//   return acc + item;
+// });
+
+// console.log(b);
+
+// const collection = {
+//   name: 'acdc',
+//   age: '1000',
+//   get name() {
+//     console.log('getter');
+//   },
+//   set age(value) {
+//     console.log('setter');
+//   },
+// };
+
+// collection.name;
+// collection.age;
+
+// class Acdc {
+//   constructor() {
+//     this.name = '艾西底系';
+//   }
+
+//   get ff() {
+//     console.log('getter');
+//     return this.name;
+//   }
+//   set f(value) {
+//     console.log('set');
+//     return (this.name = value);
+//   }
+// }
+
+// const acdc = new Acdc();
+
+function throttle(fn, wait, type) {
+  let preious, timer;
+  switch (type) {
+    case 1:
+      preious = 0;
+      break;
+    case 2:
+      timer = null;
+      break;
+  }
+
+  return () => {
+    if (type === 1) {
+      let now = Date.now();
+      if (now - preious > wait) {
+        fn.apply(this, arguments);
+        preious = now;
+      }
+    }
+    if (type === 2) {
+      if (!timer) {
+        setTimeout(() => {
+          fn.apply(this, arguments);
+          timer = null;
+        }, wait);
+      }
+    }
+  };
+}
+
+function debounce(fn, delay = 1000, immediate) {
+  let timer = null;
+  return (...args) => {
+    timer && clearTimeout(timer);
+    if (immediate) {
+      let callNow = !timer;
+      timer = setTimeout(() => {
+        timer = null;
+      }, delay);
+    }
+  };
+}
